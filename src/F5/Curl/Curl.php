992c89db05986f5930c30d20e8b3ae91ae346a08
Daniel Bonatti <danbonattis@gmail.com>
2017-05-17 15:20:36 -0300

    Curl Class

---
+++ src/F5/Curl/Curl.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+ * @author Daniel Bonatti <daniel@f5sg.com.br>
+ */
+class Curl {
+
+
+	/**
+	 * get data of $url by Curl
+	 * @param type $url 
+	 * @return String
+	 */
+	public static function getData($url) {
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 8);
+		$data = curl_exec($ch);
+
+		curl_close($ch);
+
+		return $data;
+	}
+	
+
+}
